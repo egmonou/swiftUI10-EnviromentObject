d1ea032debd2a9af7bb8d243780928ef54f6aeb5
Nour <mohamednour25@gmail.com>
2024-09-22 01:20:03 +0300

    Initial Commit

---
+++ EnviromentObject/EnviromentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnviromentObjectApp.swift
+//  EnviromentObject
+//
+//  Created by administrator on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct EnviromentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
