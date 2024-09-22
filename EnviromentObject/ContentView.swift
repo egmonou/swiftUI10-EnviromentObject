e1310eb68672cf95d790701ea6cb321314bac089
Nour <mohamednour25@gmail.com>
2024-09-22 21:15:31 +0300

    127

--- EnviromentObject/ContentView.swift
+++ EnviromentObject/ContentView.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var userViewModel: userViewMOdel
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+   
+                Text("ContentView: \(userViewModel.name)")
+                    .font(.title)
+                NavigationLink("update data", destination: UserDataView())
+            }
+            .padding()
+            .navigationTitle("Parnet View")
+        }
+    }
+}
+
+
+struct UserDataView: View {
+    @EnvironmentObject var userViewModel: userViewMOdel
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Name: \(userViewModel.name)")
+                .font(.title)
+            TextField("Enter Name", text: $userViewModel.name)
+                .padding()
+                .background(Color.blue.opacity(0.3))
         }
         .padding()
+        .navigationTitle("Child View")
     }
+    
 }
 
 #Preview {
     ContentView()
+        .environmentObject(userViewMOdel())
 }
---
+++ EnviromentObject/userViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentView.swift
+//  EnviromentObject
+//
+//  Created by administrator on 22/09/2024.
+//
+
+import Foundation
+
+class userViewMOdel: ObservableObject {
+    @Published var name: String = "Nour"
+}
+
